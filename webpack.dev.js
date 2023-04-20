fece8febc00cab4fc0705f944e961ef8a5caf7ee
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-04-20 12:18:53 -0300

    chore: add webpack config

---
+++ webpack.dev.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const common = require('./webpack.common')
+const { merge } = require('webpack-merge')
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: './public'
+    },
+    historyApiFallback: true,
+    devMiddleware: {
+      writeToDisk: true
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/template.dev.html'
+    })
+  ]
+})
